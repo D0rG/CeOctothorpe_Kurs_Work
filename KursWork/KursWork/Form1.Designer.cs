50a716671226da3add65bf4d210a1a1a48e675f9
Mixail <mixail.korchikov@mail.ru>
2020-05-26 15:40:09 +0300

    Начало работы над курсовой

---
+++ KursWork/KursWork/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace KursWork
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.PrintDB = new System.Windows.Forms.Button();
+            this.SelectDB = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(6, 19);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(861, 213);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // PrintDB
+            // 
+            this.PrintDB.Location = new System.Drawing.Point(220, 236);
+            this.PrintDB.Name = "PrintDB";
+            this.PrintDB.Size = new System.Drawing.Size(75, 23);
+            this.PrintDB.TabIndex = 1;
+            this.PrintDB.Text = "Вывод";
+            this.PrintDB.UseVisualStyleBackColor = true;
+            this.PrintDB.Click += new System.EventHandler(this.PrintDB_Click);
+            // 
+            // SelectDB
+            // 
+            this.SelectDB.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.SelectDB.Items.AddRange(new object[] {
+            "Устройства в работе",
+            "Законченые устройства"});
+            this.SelectDB.Location = new System.Drawing.Point(6, 238);
+            this.SelectDB.Name = "SelectDB";
+            this.SelectDB.Size = new System.Drawing.Size(208, 21);
+            this.SelectDB.TabIndex = 2;
+            this.SelectDB.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dataGridView1);
+            this.groupBox1.Controls.Add(this.PrintDB);
+            this.groupBox1.Controls.Add(this.SelectDB);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(873, 267);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Панель вывода базы данных";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(897, 922);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button PrintDB;
+        private System.Windows.Forms.ComboBox SelectDB;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ KursWork/KursWork/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SQLite;
+using System.Data;
+using System.IO;
+
+namespace KursWork
+{
+    public partial class Form1 : Form
+    {
+        private String dbFileName = "DataBase.sqllite";
+        private SQLiteConnection dbConnection;
+        private SQLiteCommand dbCommand;
+
+        private enum tables
+        {
+            DeviceInWork,
+            FinishedWork
+        };
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            if(!File.Exists(dbFileName)) //Если с программой нет БД, то выкидываем ошибку и вырубаем приложение.
+            {
+                MessageBox.Show("Файла базы данных не существует, приложение будет остановелно.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Environment.Exit(0);
+            }
+            else
+            {
+                dbConnection = new SQLiteConnection("Data Source=" + dbFileName);
+                dbConnection.Open();
+            }
+            SelectDB.SelectedIndex = 0;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dbCommand = new SQLiteCommand();
+            DrawTable("DeviceInWork");
+        }
+
+        private void DrawTable(string tableName)
+        {
+            dataGridView1.Columns.Clear(); //Отчистка таблицы на форме.
+            dataGridView1.Refresh();
+            SQLiteCommand command = new SQLiteCommand($"SELECT * FROM {tableName}", dbConnection);
+            SQLiteDataReader reader = null;
+            reader = command.ExecuteReader();
+            dataGridView1.Columns.Add("", "Номер модели");
+            dataGridView1.Columns.Add("", "Статус");
+            dataGridView1.Columns.Add("", "Время приёма");
+            while (reader.Read())
+            {
+                string[] fileds = new string[reader.FieldCount];
+                for(int i = 0; i < reader.FieldCount; ++i)
+                {
+                    fileds[i] = reader[i].ToString();
+                }
+                dataGridView1.Rows.Add(fileds);
+            }
+            reader.Close();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PrintDB_Click(object sender, EventArgs e)
+        {
+            DrawTable(Enum.GetName(typeof(tables), SelectDB.SelectedIndex));
+        }
+    }
+}
